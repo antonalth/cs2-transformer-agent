06f9cefe768074a4aa5f1b6a79cd1ebc4aed8219
kping0 <36621476+kping0@users.noreply.github.com>
2025-08-15 21:15:31 +0200

    Update start_cs2.bat

--- scripts/start_cs2.bat
+++ scripts/start_cs2.bat
@@ -2,4 +2,4 @@ cd "C:\Program Files (x86)\HLAE"
 hlae.exe -customLoader -noGui -autoStart ^
 -hookDllPath "C:\Program Files (x86)\HLAE\x64\AfxHookSource2.dll" ^
 -programPath "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\bin\win64\cs2.exe" ^ 
--cmdLine "-steam -insecure +sv_lan 1 -sw -w -console -w 640 -h 480 +mirv_script_load \"C:\Users\sus_things\Desktop\advancedfx-main\misc\mirv-script\dist\4-advanced-websockets\index.mjs\" +r_fullscreen 0"
+-cmdLine "-steam -insecure +sv_lan 1 -sw -w -console -w 640 -h 480 +mirv_script_load \"C:\Users\unknown\Desktop\advancedfx-main\misc\mirv-script\dist\4-advanced-websockets\index.mjs\" +r_fullscreen 0"
